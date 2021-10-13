70f8f07445b5186bc6056fb2b49598426f988372
chengyu.tu <chengyu.tu@genlot.com>
2021-10-13 15:46:17 +0800

    Wager.go:为每个用户开启一个协程，投注指定数量票

--- src/Lottery/Wager.go
+++ src/Lottery/Wager.go
@@ -12,8 +12,11 @@ import (
 )
 
 /*请求url*/
-var player_host = "127.0.0.1:9999"   /*用户管理平台*/
-var lottery_host = "127.0.0.1:12220" /*游戏引擎*/
+//var player_host = "127.0.0.1:9999"   /*用户管理平台*/
+//var lottery_host = "127.0.0.1:12220" /*游戏引擎*/
+
+var player_host = "10.9.0.128:9999"  /*用户管理平台*/
+var lottery_host = "10.9.0.87:12220" /*游戏引擎*/
 
 var url_map = map[string]string{
 	"player_login":   "http://" + player_host + "/user/un/login",
@@ -44,15 +47,6 @@ var player_set = []player{
 	{"100001256", "13800000036", "14e1b600b1fd579f47433b88e8d85291", ""},
 }
 
-/*投注总票数*/
-var (
-	total_ticket int
-	mutex        sync.Mutex
-)
-
-/*票流水号*/
-var tickSN int64
-
 //用户登陆，获取token
 /*用户登陆返回数据结构
 type tmp struct{
@@ -63,7 +57,7 @@ type tmp struct{
 	success bool
 }
 */
-func player_login(count int) {
+func login_post(val player, k int) {
 	url := url_map["player_login"]
 	loginData := map[string]interface{}{
 		"loginType":         0,
@@ -75,44 +69,49 @@ func player_login(count int) {
 		"password":          "",
 		"timestamp":         "",
 	}
-	for k, val := range player_set {
-		//达到请求数量，退出
-		if k == count {
-			break
+
+	//初始化客户端
+	client := &http.Client{}
+
+	//填充请求参数
+	loginData["playerAccount"] = val.playerID
+	loginData["password"] = val.passwd
+	loginData["timestamp"] = time.Now().Unix()
+
+	//请求map转换为json
+	data, _ := json.Marshal(loginData)
+	//fmt.Println(string(data))
+
+	//发起http请求
+	requestData := strings.NewReader(string(data))
+	req, _ := http.NewRequest("POST", url, requestData)
+	req.Header.Add("Content-Type", "application/json")
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Println("PlayerCode: ", val.playerID, "login error, ", err)
+		return
+	}
+	body, _ := ioutil.ReadAll(response.Body)
+	//fmt.Println(string(body))
+
+	//取出结果中的token，填充用户结构体
+	result := make(map[string]interface{})
+	json.Unmarshal(body, &result)
+	//fmt.Println(result)
+	player_set[k].token = fmt.Sprintf("%v", result["data"])
+
+	//关闭连接
+	response.Body.Close()
+}
+
+//参数说明：index == -1：登陆所有用户；index >= 0：登陆指定用户
+func player_login(index int) {
+	if index == -1 {
+		for k, val := range player_set {
+			login_post(val, k)
 		}
-
-		//初始化客户端
-		client := &http.Client{}
-
-		//填充请求参数
-		loginData["playerAccount"] = val.playerID
-		loginData["password"] = val.passwd
-		loginData["timestamp"] = time.Now().Unix()
-
-		//请求map转换为json
-		data, _ := json.Marshal(loginData)
-		//fmt.Println(string(data))
-
-		//发起http请求
-		requestData := strings.NewReader(string(data))
-		req, _ := http.NewRequest("POST", url, requestData)
-		req.Header.Add("Content-Type", "application/json")
-		response, err := client.Do(req)
-		if err != nil {
-			fmt.Println("PlayerCode: ", val.playerID, "login error, ", err)
-			return
-		}
-		body, _ := ioutil.ReadAll(response.Body)
-		//fmt.Println(string(body))
-
-		//取出结果中的token，填充用户结构体
-		result := make(map[string]interface{})
-		json.Unmarshal(body, &result)
-		//fmt.Println(result)
-		player_set[k].token = fmt.Sprintf("%v", result["data"])
-
-		//关闭连接
-		response.Body.Close()
+	} else {
+		login_post(player_set[index], index)
 	}
 }
 
@@ -175,7 +174,7 @@ func get_game_issue(game_id int, wager_issue *int) {
 var wg sync.WaitGroup
 
 //投注
-func wager(game_id, wager_issue, index int) {
+func wager(game_id, wager_issue, index, startSN, count int, ticketCount *int) {
 	url := url_map["wager"]
 	//填充请求信息
 	request := map[string]interface{}{
@@ -281,65 +280,67 @@ func wager(game_id, wager_issue, index int) {
 
 	val := player_set[index]
 
-	//初始化客户端
-	client := &http.Client{}
-
 	//填充请求参数
 	//data["Format01"]["UserID"] type interface {} does not support indexing，需要使用类型断言，将data["Format01"]转换为map[string]interface{}类型
 	request["Format01"].(map[string]interface{})["UserID"] = val.playercode
-	request["Format01"].(map[string]interface{})["TimeStamp"] = string(time.Now().Unix())
 	request["Format01"].(map[string]interface{})["Access-Token"] = val.token
-	request["Format01"].(map[string]interface{})["TickSN"] = tickSN
-	//fmt.Println(request)
 
-	//请求map转换为json
-	data, _ := json.Marshal(request)
-	//fmt.Println(string(data))
+	//循环进行投注
+	for i := 0; i < count; i++ {
+		request["Format01"].(map[string]interface{})["TimeStamp"] = string(time.Now().Unix())
+		request["Format01"].(map[string]interface{})["TickSN"] = startSN
+		startSN++
 
-	//发起http请求
-	requestData := strings.NewReader(string(data))
-	req, _ := http.NewRequest("POST", url, requestData)
-	req.Header.Add("Content-Type", "application/json")
-	response, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("http post error, %v\n", err)
+		//fmt.Println(request)
+
+		//请求map转换为json
+		data, _ := json.Marshal(request)
+		//fmt.Println(string(data))
+
+		//初始化客户端
+		client := &http.Client{}
+
+		//发起http请求
+		requestData := strings.NewReader(string(data))
+		req, _ := http.NewRequest("POST", url, requestData)
+		req.Header.Add("Content-Type", "application/json")
+		response, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("http post error, %v\n", err)
+
+			//关闭连接
+			response.Body.Close()
+			wg.Done()
+			return
+		}
+		body, _ := ioutil.ReadAll(response.Body)
+		//fmt.Println(string(body))
+
+		//判断返回码是否等于0
+		var result map[string]interface{}
+		json.Unmarshal(body, &result)
+		retcode, _ := strconv.Atoi(result["RetCode"].(string))
+		if retcode == 0 {
+			*ticketCount++
+		} else {
+			fmt.Printf("GameID:%d, WagerIssue:%d, UserID:%s, RetCode:%d\n", game_id, wager_issue, val.playerID, retcode)
+		}
 
 		//关闭连接
 		response.Body.Close()
-		wg.Done()
-		return
 	}
-	body, _ := ioutil.ReadAll(response.Body)
-	//fmt.Println(string(body))
-
-	//判断返回码是否等于0
-	var result map[string]interface{}
-	json.Unmarshal(body, &result)
-	retcode, _ := strconv.Atoi(result["RetCode"].(string))
-	if retcode == 0 {
-		mutex.Lock()
-		total_ticket++
-		fmt.Println("total wager ticket count: ", total_ticket)
-		mutex.Unlock()
-	} else {
-		fmt.Printf("GameID:%d, WagerIssue:%d, UserID:%s, RetCode:%d\n", game_id, wager_issue, val.playerID, retcode)
-	}
-	//关闭连接
-	response.Body.Close()
 	wg.Done()
 }
 
 func main() {
-	count := 1
 	game_id := 11001
 	wager_issue := 0
-	total_ticket = 0
-	//tickSN = time.Now().Unix()
-	tickSN = 1
-	fmt.Println(tickSN)
+	total_ticket := make([]int, 12)
+	tickSN := 100
+	//fmt.Println(tickSN)
 
 	//用户登陆，更新token
-	player_login(count)
+	player_login(-1)
 	//fmt.Println(player_set)
 
 	//获取游戏期信息
@@ -350,14 +351,22 @@ func main() {
 	}
 
 	//循环投注
-	for index := 0; index <= 12; index++ {
-		if index == 12 {
-			index = 0
+	for index := 0; index < 12; index++ {
+		//校验用户token是否未空
+		if len(player_set[index].token) == 0 {
+			//重新登陆
+			fmt.Printf("User(%s)`s token is empty\n", player_set[index].playerID)
+			player_login(index)
 		}
 		wg.Add(1)
-		go wager(game_id, wager_issue, index)
-		tickSN++
-		time.Sleep(10 * time.Millisecond)
+		go wager(game_id, wager_issue, index, tickSN, 10, &total_ticket[index])
 	}
 	wg.Wait()
+
+	//统计总票数
+	ticketCount := 0
+	for _, val := range total_ticket {
+		ticketCount += val
+	}
+	fmt.Println("total wager ticket count: ", ticketCount)
 }
